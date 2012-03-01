744f28825039bfd1efc0e37bb8e4f0e9e33c2b46
Oleg Brodkin <olegb@linuxsquad.com>
2012-03-01 07:57:08 -0800

    fixed: UUID, hex char, utf-8

--- conv2KeePass.py
+++ conv2KeePass.py
@@ -12,6 +12,14 @@ TODO:
   Some characters have to be replaced (for instance, &) since keePass refuses to import them
   If SplashID has no username, check <comment> section for <account> data to substitute
 
+From KeePass website:
+
+Important notes about the format:
+ The files must be encoded using UTF-8 (Unicode). Other encodings are not supported.
+ The following five entities must be encoded: < > & " '. They are encoded by &lt; &gt; &amp; &quot; &apos;.
+ The UUID is a hex-encoded 16-byte string (i.e. an 32 ANSI hex character string in the XML file). It is unique (also across multiple databases) and can be used to identify entries.
+ Dates/times are encoded in the standard date/time XML format (YYYY-MM-DDTHH:mm:ss): first the date in form YYYY-MM-DD, a 'T' character, and the time in form HH:mm:ss.
+
 From SplashID website:
  You may also import and export SplashID records in CSV format.
  CSV stands for Comma Separated Values, and is a common file format
@@ -29,29 +37,42 @@ From SplashID website:
  does not match an existing type a new type will be created.
 """
 
+import codecs
 import sys
 import csv
+import uuid
+import string
+from string import maketrans 
 import time 
 from time import gmtime, strftime
 
 TODAY = strftime("%Y-%m-%dT%X",gmtime())
+#
+# NON-mutable
+SPLASHID_2_KEEPASSX  = {'DESCRIPTION': 'title', 'COMPANY': 'title', 'USERNAME': 'username',
+                        'PASSWORD': 'password', 'PIN': 'password', 'URL': 'url'}
+CHAR_2_REPLACE = {'<': '&lt;', '>': '&gt;', '&': '&amp;','\"': '&quot;', '\'': '&apos;'}
 
+#
+# mutable
 splashid_type = {}
 splashid_value = []
 splashid_type_prev = '0'
-SPLASHID_2_KEEPASSX  = {'DESCRIPTION': 'title', 'COMPANY': 'title', 'USERNAME': 'username',
-                        'PASSWORD': 'password', 'PIN': 'password', 'URL': 'url'}
-xml_entry = {'title': [1,''], 'username': [2,''], 'url': [3,''], 'password': [4,''], 'notes': [5,''], 'uuid': [6,''], 'image': [7, '67'],
+xml_entry = {'title': [1,''], 'username': [2,''], 'url': [3,''], 'password': [4,''], 'notes': [5,''], 'uuid': [6,''], 'image': [7, '0'],
         'creationtime': [8,''], 'lastmodtime': [9,TODAY],'lastaccesstime': [91,TODAY] }
 xml_group = {'title': ''}
 
+#
+#
+# 
 if (len(sys.argv) > 1):
     splashid_export_f = sys.argv[1]
 else:
     print "Please supply splashid_export_f name"
     quit(1)
 
-with open(splashid_export_f, 'rb') as csv_file:
+#with codecs.open(splashid_export_f, "rb", "utf-8") as csv_file:
+with open(splashid_export_f, "rb") as csv_file:
     csv_imported_splashid = csv.reader(csv_file)
     with open(splashid_export_f+".log", 'w') as log_file:
         for i, row in enumerate(csv_imported_splashid):
@@ -77,6 +98,15 @@ with open(splashid_export_f, 'rb') as csv_file:
 
 splashid_value.sort()
 
+def replace_character(string):
+    mod_string = ""    
+    for i,char in enumerate(string):
+        if char in CHAR_2_REPLACE:
+            mod_string += CHAR_2_REPLACE[char]
+        else:
+            mod_string += char
+    return(mod_string)
+            
 print '<?xml version="1.0" encoding="UTF-8"?>'
 print "<pwlist>"
 
@@ -86,7 +116,7 @@ for single_record in splashid_value:
     if len(single_record) > 0:
         if  single_record[0] in splashid_type: 
             field_names = splashid_type[single_record[0]]
-
+#            print single_record
                     
             xml_entry['notes'][1] = ''
 
@@ -96,11 +126,11 @@ for single_record in splashid_value:
                     index = item.strip('0123456789 ').upper()
                     if index in SPLASHID_2_KEEPASSX :
                         if SPLASHID_2_KEEPASSX[index] == "title":
-                            xml_entry[SPLASHID_2_KEEPASSX[index]][1] = single_record[j]+" ("+single_record[-2]+")"
+                            xml_entry[SPLASHID_2_KEEPASSX[index]][1] = '{!s} ({!s})'.format(single_record[j], single_record[-2])
                         else:
                             xml_entry[SPLASHID_2_KEEPASSX[index]][1] = single_record[j]
                     else:
-                        xml_entry['notes'][1] += item+": "+single_record[j]+".\n"
+                        xml_entry['notes'][1] += '{!s}: {!s}.\n'.format(item, repr(single_record[j]))
                 except IndexError, name:
                     print "Index ERROR", name
 
@@ -113,13 +143,16 @@ for single_record in splashid_value:
                     print "  ERR: ", name
                     quit(1)
 
-            xml_entry['notes'][1] += "comment: "+single_record[-1]+"."
+            xml_entry['notes'][1] += 'comment: {!s}.'.format(repr(single_record[-1]))
             
             print "  <pwentry>"
             print "     <group>{}</group>".format(field_names[0])
 
             for item in sorted( (value,field) for (field,value) in xml_entry.items() ):
-                print "     <{0}>{1}</{0}>".format(item[1],item[0][1])
+                if item[1] is 'uuid':
+                    print "     <{0}>{1}</{0}>".format(item[1],str(uuid.uuid1()).replace('-',''))
+                else:                    
+                    print "     <{0}>{1}</{0}>".format(item[1], replace_character(item[0][1]).encode("utf-8"))
             print '     <expiretime expires="false">2999-12-28T23:59:59</expiretime>'
             print "  </pwentry>"
             
